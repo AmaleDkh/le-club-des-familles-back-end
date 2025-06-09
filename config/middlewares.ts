84394d4efa2d5f98747c4b15be82764acddb47ad
Amale Dkhissi <amdkhissi@gmail.com>
2025-06-09 20:59:17 +0200

    middlewares configuration

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,18 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+export default ({ env }) => [
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: [env("FRONTEND_URL"), "http://localhost:3000"],
+      credentials: true,
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];

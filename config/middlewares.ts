d61b3debecabfc32eca5ecdb398b5ce6a3853180
Amale Dkhissi <amdkhissi@gmail.com>
2025-07-01 10:29:02 +0200

    change middlewares config

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,21 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: [
+        "https://le-club-des-familles-git-main-amaledkhs-projects.vercel.app",
+      ],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+      headers: "*",
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];

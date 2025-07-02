565bc2e6f2903f010940df8c844bdf06df70b4f9
Amale Dkhissi <amdkhissi@gmail.com>
2025-07-02 14:49:09 +0200

    change database config

--- config/database.ts
+++ config/database.ts
@@ -7,32 +7,17 @@ export default ({ env }) => {
     postgres: {
       connection: {
         connectionString: env("DATABASE_URL"),
-        ssl: env.bool("DATABASE_SSL", false)
-          ? {
-              rejectUnauthorized: env.bool(
-                "DATABASE_SSL_REJECT_UNAUTHORIZED",
-                true
-              ),
-              key: env("DATABASE_SSL_KEY", undefined),
-              cert: env("DATABASE_SSL_CERT", undefined),
-              ca: env("DATABASE_SSL_CA", undefined),
-              capath: env("DATABASE_SSL_CAPATH", undefined),
-              cipher: env("DATABASE_SSL_CIPHER", undefined),
-            }
-          : false,
-
-        // ssl: env.bool("DATABASE_SSL", false) && {
-        //   rejectUnauthorized: env.bool(
-        //     "DATABASE_SSL_REJECT_UNAUTHORIZED",
-        //     true
-        //   ),
-        //   key: env("DATABASE_SSL_KEY", undefined),
-        //   cert: env("DATABASE_SSL_CERT", undefined),
-        //   ca: env("DATABASE_SSL_CA", undefined),
-        //   capath: env("DATABASE_SSL_CAPATH", undefined),
-        //   cipher: env("DATABASE_SSL_CIPHER", undefined),
-        // },
-        schema: env("DATABASE_SCHEMA", "public"),
+        // ssl: env.bool("DATABASE_SSL", false)
+        //   ? {
+        //       rejectUnauthorized: env.bool(
+        //         "DATABASE_SSL_REJECT_UNAUTHORIZED",
+        //         true
+        //       ),
+        //     }
+        //   : false,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
       pool: {
         min: env.int("DATABASE_POOL_MIN", 2),
@@ -60,6 +45,57 @@ export default ({ env }) => {
     },
   };
 };
+
+// import path from "path";
+
+// export default ({ env }) => {
+//   const client = env("DATABASE_CLIENT", "sqlite");
+
+//   const connections = {
+//     postgres: {
+//       connection: {
+//         connectionString: env("DATABASE_URL"),
+//         ssl: env.bool("DATABASE_SSL", false)
+//           ? {
+//               rejectUnauthorized: env.bool(
+//                 "DATABASE_SSL_REJECT_UNAUTHORIZED",
+//                 true
+//               ),
+//               key: env("DATABASE_SSL_KEY", undefined),
+//               cert: env("DATABASE_SSL_CERT", undefined),
+//               ca: env("DATABASE_SSL_CA", undefined),
+//               capath: env("DATABASE_SSL_CAPATH", undefined),
+//               cipher: env("DATABASE_SSL_CIPHER", undefined),
+//             }
+//           : false,
+//         schema: env("DATABASE_SCHEMA", "public"),
+//       },
+//       pool: {
+//         min: env.int("DATABASE_POOL_MIN", 2),
+//         max: env.int("DATABASE_POOL_MAX", 10),
+//       },
+//     },
+//     sqlite: {
+//       connection: {
+//         filename: path.join(
+//           __dirname,
+//           "..",
+//           "..",
+//           env("DATABASE_FILENAME", ".tmp/data.db")
+//         ),
+//       },
+//       useNullAsDefault: true,
+//     },
+//   };
+
+//   return {
+//     connection: {
+//       client,
+//       ...connections[client],
+//       acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+//     },
+//   };
+// };
 
 // import path from 'path';
 
